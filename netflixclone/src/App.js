7bc7a918e2c5ab7576ac3c2bc88928f08b2d2259
Luis Rodrigues <luis_henrique993@live.com>
2021-08-19 16:30:15 -0300

    [FEAT] firtst page

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,21 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css'
 import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
 
+  const [movieList, setMovieList] = useState([])
+
   useEffect(()=>{
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
     }
 
     loadAll();
   },[]);
 
   return (
-    <div>
-      Teste
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
---
+++ netflixclone/src/components/MovieRow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './MovieRow.css';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({title, items}) => {
+    return (
+        <div className="moviewRow">
+            <h2>{title}</h2>
+            <div className="movieRow--listarea">
+                <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
