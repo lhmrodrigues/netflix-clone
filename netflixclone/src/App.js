bb061204cb7d1b070a198452198e3754d7dc0743
Luis Rodrigues <luis_henrique993@live.com>
2021-08-19 19:57:38 -0300

    [FEAT] Final commit

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -59,6 +59,11 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤</span> por Luis Rodrigues <br/>
+        Direitos de imagem para Netflix <br/>
+        Dados pegos do site Themoviedb.org
+      </footer>
     </div>
   );
 }
--- netflixclone/src/Tmdb.js
+++ netflixclone/src/Tmdb.js
@@ -26,31 +26,31 @@ export default {
                 title: 'Em Alta',
                 items: await basicFetch(`/movie/top_rated?`)
             },
-            {
-                slug: 'action',
-                title: 'Ação',
-                items: await basicFetch(`/discover/movie?with_genres=28?`)
-            },
-            {
-                slug: 'comedy',
-                title: 'Comédia',
-                items: await basicFetch(`/discover/movie?with_genres=35?`)
-            },
-            {
-                slug: 'horror',
-                title: 'Terror',
-                items: await basicFetch(`/discover/movie?with_genres=27?`)
-            },
-            {
-                slug: 'romance',
-                title: 'Romance',
-                items: await basicFetch(`/discover/movie?with_genres=10749?`)
-            },
-            {
-                slug: 'documentary',
-                title: 'Documentário',
-                items: await basicFetch(`/discover/movie?with_genres=99?`)
-            },
+            // {
+            //     slug: 'action',
+            //     title: 'Ação',
+            //     items: await basicFetch(`/discover/movie?with_genres=28?`)
+            // },
+            // {
+            //     slug: 'comedy',
+            //     title: 'Comédia',
+            //     items: await basicFetch(`/discover/movie?with_genres=35?`)
+            // },
+            // {
+            //     slug: 'horror',
+            //     title: 'Terror',
+            //     items: await basicFetch(`/discover/movie?with_genres=27?`)
+            // },
+            // {
+            //     slug: 'romance',
+            //     title: 'Romance',
+            //     items: await basicFetch(`/discover/movie?with_genres=10749?`)
+            // },
+            // {
+            //     slug: 'documentary',
+            //     title: 'Documentário',
+            //     items: await basicFetch(`/discover/movie?with_genres=99?`)
+            // },
         ];
     },
 
